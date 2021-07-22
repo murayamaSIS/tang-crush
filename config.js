b3ff404b341b3975091d873c9053d4d0cdfc5b03
murayamaSIS <87360755+murayamaSIS@users.noreply.github.com>
2021-07-22 13:44:47 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'phương nghi ơi !',
+    introTitle: 'quỳnh như  ơi !',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
